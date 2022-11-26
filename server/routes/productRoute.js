e3cb59864f416270647c460a845e02cab0969c60
NakkaGS <gtnakata@gmail.com>
2022-11-26 23:33:07 +0100

    Testing new stuff

--- server/routes/productRoute.js
+++ server/routes/productRoute.js
@@ -2,28 +2,27 @@ const express = require("express");
 const router = express.Router();
 const Product = require('../models/productModel')
 
-
+//Get all Products
 router.get("/getallproducts", (req, res) => {
 
-     Product.find({} , (err , docs)=>{
+    Product.find({} , (err , docs)=>{
 
-               if(!err)
-               {
-                   return res.send(docs);
-               }
-               else{
-                   return res.status(400).json({ message: 'Something went wrong' });
-               }
+    if(!err)
+    {
+        return res.send(docs);
+    }
+    else{
+        return res.status(400).json({ message: 'Something went wrong' });
+    }
 
-     })
+    })
   
 });
 
-router.post("/getproductbyid/", (req, res) => {
-    //res.send(req.params)
-    console.log(res)
+//Get Product ID by POST using Body
+router.post("/getproductbyid", (req, res) => {
 
-    Product.find({_id : req.body.id} , (err , docs)=>{
+    Product.find({_id : req.body.productid} , (err , docs)=>{
 
         if(!err)
         {
--- server/server.js
+++ server/server.js
@@ -1,22 +1,23 @@
+//Express
 const express = require("express")
-
 const app = express()
 
+//Cors - Cross-Origin Resource Sharing 
 var cors = require('cors')
 
+//Call the db.js and start the server
 var dbconnection = require('./db')
+
+//Routes
 var productsRoute = require('./routes/productRoute')
 
+//Create Route
 app.use('/api/products/' , productsRoute, cors())
 
 app.get("/", (req, res) => {
     res.send("This is from backend")
 })
 
-const port = 5000;
-
-app.listen(port, () => console.log("Node JS Server Started"))
-
 app.listen(8000, function () {
-    console.log('Example app listening on port 8000!');
+    console.log('Node JS Server Started on Port 8000!');
    });
