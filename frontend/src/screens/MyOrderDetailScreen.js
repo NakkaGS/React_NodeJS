f4c8a3c745222629a6581383e3461df2a97ab6fb
NakkaGS <gtnakata@gmail.com>
2023-01-02 23:15:10 +0100

    DetailOrder styling not done

--- frontend/src/screens/MyOrderDetailScreen.js
+++ frontend/src/screens/MyOrderDetailScreen.js
@@ -1,12 +1,16 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 import { useDispatch, useSelector } from "react-redux";
 
 import { useNavigate, useParams } from 'react-router-dom'
 
 import { myOrder } from '../actions/orderActions'
+//Boostrap Components
+import { Button, Badge, Form } from 'react-bootstrap'
 
 function MyOrderDetailScreen({ match }) {
+
+  const[delivered,setDelivered] = useState(false)
 
   let history = useNavigate();
 
@@ -18,8 +22,6 @@ function MyOrderDetailScreen({ match }) {
   const orderMy = useSelector((state) => state.orderMy)
   const { loading, error, order  } = orderMy
 
-
-
   let { id } = useParams(match); //get the Product ID
 
   useEffect (() => {
@@ -27,20 +29,37 @@ function MyOrderDetailScreen({ match }) {
       history('/login')
     } else {
       dispatch(myOrder(id))
-
-      console.log(order[0])
+      setDelivered(order.isDelivered)
     }
   }, [dispatch, history, id])
 
   return (
-    <div>
-      <div className="orderhead">
-        <div className="row">
+    <div className="container">
+        <div className="row detail-head">
           <div className="col">
-            <h2>Order # </h2>
+            <h3>Order <strong>#{order._id}</strong></h3>
+            {order.isDelivered ? <Badge bg="success">Success</Badge> : <Badge bg="warning" text="dark"><storng>Not Delivered</storng></Badge>}
+          </div>
+          <div className="col head-left">
+
+            <Form.Group className="orderhead" controlId="brand">
+                <Form.Control
+                  as='select'
+                  value={delivered} //it must have the same name as in the database attribute
+                  onChange={(e) => setDelivered(e.target.value)}>
+                      <option value="false">Not Delivered</option>
+                      <option value="true">Delivered</option>
+                </Form.Control>
+
+                <Button type="submit" varint='Primary'>
+              Save
+            </Button>
+            </Form.Group>
+
+
           </div>
         </div>
-      </div>
+
 
     </div>
   )
