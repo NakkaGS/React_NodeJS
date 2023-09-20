06d9881c30c4925a99b192933afa9349a623094c
NakkaGS <gtnakata@gmail.com>
2023-09-20 21:02:28 +0200

    Update weatherRoute.js

--- server/routes/weatherRoute.js
+++ server/routes/weatherRoute.js
@@ -8,13 +8,14 @@ router.use(bodyParser.json())
 router.use(bodyParser.urlencoded({ extended: true }))
 
 // Define the API endpoint you want to consume
-const currentWeatherUrl = `https://api.weatherapi.com/v1/current.json?key=` + process.env.REACT_APP_WEATHER_API_KEY ;
-const forecastWeatherUrl = `https://api.weatherapi.com/v1/current.json?key=` + process.env.REACT_APP_WEATHER_API_KEY ;
+const currentWeatherUrl = `https://api.weatherapi.com/v1/current.json?key=` + process.env.REACT_APP_WEATHER_API_KEY + '&aqi=no&q=';
+const forecastWeatherUrl = `https://api.weatherapi.com/v1/current.json?key=` + process.env.REACT_APP_WEATHER_API_KEY + '&days=3&aqi=no&alerts=no&q=';
 
 // Define a route to fetch and send data from the API to the frontend
 router.get('/getcurrentweather', async (req, res) => {
   try {
-    const response = await axios.get(currentWeatherUrl + '&q=' {req.body.city} + '&aqi=no');
+    const currentWeatherUrlComplete = currentWeatherUrl + req.body.city;
+    const response = await axios.get(currentWeatherUrlComplete);
     const apiData = response.data;
 
     // You can process the data here if needed
@@ -30,7 +31,8 @@ router.get('/getcurrentweather', async (req, res) => {
 // Define a route to fetch and send data from the API to the frontend
 router.get('/getforecastweather', async (req, res) => {
   try {
-    const response = await axios.get(forecastWeatherUrl + '&q=' {req.body.city} + '&days=3&aqi=no&alerts=no');
+    const forecastWeatherUrlComplete = forecastWeatherUrl + req.body.city;
+    const response = await axios.get(forecastWeatherUrlComplete);
     const apiData = response.data;
 
     // You can process the data here if needed
